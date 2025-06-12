7042bd71b1cb4c728faf7f729d5093ec6a4128d2
João Pedro Gomes <joaop.gomes@usp.br>
2025-06-12 11:26:17 -0300

    Implement QR code scanning functionality with tree detection and success page

--- flaskr/app.py
+++ flaskr/app.py
@@ -9,5 +9,15 @@ def serve_index():
     print(root_dir)
     return send_from_directory(root_dir, 'index.html')
 
+@app.route('/search')
+def serve_search():
+    root_dir = os.path.dirname(os.path.abspath(__file__)) + "/frontend"
+    return send_from_directory(root_dir, 'search.html')
+
+@app.route('/success')
+def serve_success():
+    root_dir = os.path.dirname(os.path.abspath(__file__)) + "/frontend"
+    return send_from_directory(root_dir, 'success.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
